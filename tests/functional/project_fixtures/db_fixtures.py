bf2f82bbbe00d6e6a9feeaab11d92a2c11d3ca09
Антон Ларионов <tuppey_tony@MacBook-Pro-Anton.local>
2024-05-24 11:20:28 +0300

    Добавил запуск тестов при прикоммите.

--- tests/functional/project_fixtures/db_fixtures.py
+++ tests/functional/project_fixtures/db_fixtures.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+from typing import Any
 from typing import AsyncGenerator
 from typing import Optional
 
+import docker
+import docker.errors
+import pytest
 import pytest_asyncio
 
+from docker.models.containers import Container
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
 
 from src.db.postgres import get_session
 from src.main import app
@@ -12,14 +19,10 @@ from src.models import AuthUserModel
 from src.models import Base
 from tests.functional.settings import tests_settings
 from tests.functional.testdata.database_mapping import database_data
+from tests.functional.utils.utils import wait_until_docker_healthy
 
 
 async_session: Optional[AsyncSession] = None
-dsn = (
-    f'{tests_settings.db_driver}://{tests_settings.db_user}:{tests_settings.db_password}'
-    f'@{tests_settings.db_host}:{tests_settings.db_port}/{tests_settings.db_name}'
-)
-engine_test = create_async_engine(dsn, echo=True, future=True)
 
 
 async def override_get_session() -> AsyncSession:  # type: ignore
@@ -31,15 +34,53 @@ async def override_get_session() -> AsyncSession:  # type: ignore
 app.dependency_overrides[get_session] = override_get_session
 
 
+@pytest.fixture(scope='session')
+def create_db() -> Any:
+    """Создания докер контейнера для тестовой базы."""
+    client = docker.from_env()
+    path_to_schema = Path.cwd() / 'db_schema.sql'
+    postgres = client.containers.run(
+        'postgres:13',
+        environment={
+            'POSTGRES_PASSWORD': tests_settings.db_password,
+            'POSTGRES_USER': tests_settings.db_user,
+            'POSTGRES_DB': tests_settings.db_name,
+        },
+        volumes=[f'{path_to_schema.absolute()}:/docker-entrypoint-initdb.d/db_schema.sql'],
+        ports={
+            '5432/tcp': int(tests_settings.db_port),
+        },
+        healthcheck={
+            'test': ['CMD-SHELL', f"sh -c 'pg_isready -U {tests_settings.db_user} -d {tests_settings.db_name}'"],
+            'interval': 10 * 1_000_000_000,
+            'timeout': 10 * 1_000_000_000,
+            'retries': 5,
+        },
+        detach=True,
+    )
+    wait_until_docker_healthy(postgres, client)
+    return postgres
+
+
+@pytest.fixture(scope='session')
+def db_engine() -> AsyncEngine:
+    """Создания движка для ОРМ."""
+    dsn = (
+        f'{tests_settings.db_driver}://{tests_settings.db_user}:{tests_settings.db_password}'
+        f'@{tests_settings.db_host}:{tests_settings.db_port}/{tests_settings.db_name}'
+    )
+    return create_async_engine(dsn, echo=True, future=True)
+
+
 @pytest_asyncio.fixture(autouse=True, scope='session')
-async def _prepare_db() -> AsyncGenerator:
+async def _prepare_db(create_db: Container, db_engine: AsyncEngine) -> AsyncGenerator:
     """Подготовка БД для тестов."""
-    async with engine_test.begin() as conn:
+    async with db_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    async with AsyncSession(engine_test) as session:
+    async with AsyncSession(db_engine) as session:
         async with session.begin():
-            session.add_all([AuthUserModel(**user) for user in database_data])
+            session.add_all([AuthUserModel(**user) for user in database_data])  # type: ignore[arg-type]
         await session.commit()
     yield
-    async with engine_test.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+    create_db.stop()
+    create_db.remove()
